0bcfeb4b39a958a06a435b99dfecac98aa7c2dce
Brodie Slater <brodie.slater@contino.io>
2018-05-11 14:08:56 +0100

    Initial commit

---
+++ test_app.py
@@ -0,0 +1,9 @@
+import unittest
+
+import app
+
+class TestApp(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(app.add(10, 5), 15)
+        self.assertEqual(app.add(-2, 2), 0)

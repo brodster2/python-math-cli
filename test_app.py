732a271446304c8b33d82d5448f50faf051dcf24
Brodie Slater <brodie.slater@contino.io>
2018-06-15 13:49:09 +0100

    Intentional test failure to check alerting

--- test_app.py
+++ test_app.py
@@ -6,4 +6,4 @@ class TestApp(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(app.add(10, 5), 15)
-        self.assertEqual(app.add(-2, 2), 0)
+        self.assertEqual(app.add(-2, 2), 1)
